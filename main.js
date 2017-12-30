aa5235909c38f83038d3d0368876864aab2295ff
KristianJakubik <jakubikkristian@gmail.com>
2017-12-30 03:09:45 +0100

    Implementation of phantom module finished

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets */
 
-/** Simple extension that adds a "File > Hello World" menu item. Inserts "Hello, world!" at cursor pos. */
+/** Simple extension that adds a "File > Hello World" menu 
+item. Inserts "Hello, world!" at cursor pos. */
+
 define(function (require, exports, module) {
-    "use strict";
+	"use strict";
 
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        EditorManager  = brackets.getModule("editor/EditorManager"),
-        Menus          = brackets.getModule("command/Menus");
+	const NodeDomain = brackets.getModule("utils/NodeDomain");
+	const ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+	const phantom = new NodeDomain("phantom", ExtensionUtils.getModulePath(module, "node/node-phantom"));
 
-    
-    // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        var editor = EditorManager.getFocusedEditor();
-        if (editor) {
-            var insertionPos = editor.getCursorPos();
-            editor.document.replaceRange("Hello, world!", insertionPos);
-        }
-    }
-    
-    
-    // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.writehello";   // package-style naming to avoid collisions
-    CommandManager.register("Hello World 2", MY_COMMAND_ID, handleHelloWorld);
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
+
+	//	console.log(phantom);
+
+	const testKata = 'consecutive-strings';
+	const url = `https://www.codewars.com/kata/${testKata}/train/javascript`;
+	const apiAccessToken = "nzEBMyszeCXhpzGdT_cS";
+
+	let CommandManager = brackets.getModule("command/CommandManager");
+	let EditorManager = brackets.getModule("editor/EditorManager");
+	let Menus = brackets.getModule("command/Menus");
+
+	function parseHTML(html) {
+		console.log(html);
+	}
+
+	function main() {
+		let CustomHeaders = {
+			"Authorize": apiAccessToken
+		};
+		phantom.exec("phantomWebPage", url, CustomHeaders)
+			.done(text => parseHTML(text))
+			.fail(error => console.log(`ERORROR: ${error}`));
+	}
+
+	// First, register a command - a UI-less object associating an id to a handler
+	var MY_COMMAND_ID = "codewar"; // package-style naming to avoid collisions
+	CommandManager.register("Codewar", MY_COMMAND_ID, main);
+
+	// Then create a menu item bound to the command
+	// The label of the menu item is the name we gave the command (see above)
+	var menu = Menus.addMenu("Codewar", MY_COMMAND_ID);
+	//menu.addMenuItem(MY_COMMAND_ID);
+
+	let contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
+	contextMenu.addMenuItem(MY_COMMAND_ID);
 });
--- node/node-phantom.js
+++ node/node-phantom.js
@@ -1,65 +1,48 @@
 (function () {
 	'use strict';
+
 	const phantom = require('phantom');
 	const os = require('os');
+	let error_message = "";
 
-	//	function phantomWebPageA() {
-	//		const instance = await phantom.create();
-	//		const page = await instance.createPage();
-	//
-	//		const status = await page.open('https://stackoverflow.com/');
-	//		const content = await page.property('content');
-	//		await instance.exit();
-	//		return content;
-	//	}
+	function phantomWebPage(pageToOpen, customHeaders, callback) {
 
-	function phantomWebPage() {
 		let instance = phantom.create();
+		instance.customHeaders = customHeaders;
+		
 		let page = instance
-			.then(instance => instance.createPage())
+			.then(instance => instance.createPage());
 
 		let content = page
-			.then(page => page.open('https://stackoverflow.com/'))
-			.then(status => {
-				console.log(`STATUS: ${status}`);
-				return page.then(page => page.property('content'))
-			})
-			.catch(error => console.log(`ERROR2 ${error}`));
+			.then(page => page.open(pageToOpen))
+			.then(status => page.then(page => page.property('content')))
+			.catch(error => callback(error));
 
 		content
-			.then(() => {
-				console.log("KONIEC FUNKCIE");
-			return instance.then(instance => instance.exit());
-		})
-			.catch(error => console.log(`ERROR3 ${error}`));
-		return content;
+			.then(() => instance.then(instance => instance.exit()))
+			.catch(error => callback(error));
+///TODO: Better optimalization
+		content
+			.then(text => callback(null, text))
+			.catch(error => callback(error));
 	}
-
-	function cmdGetMemory(total) {
-		if (total) {
-			return os.totalmem();
-		} else {
-			return os.freemem();
-		}
-	}
-
 
 	/**
 	 * Initializes the test domain with several test commands.
 	 * @param {DomainManager} domainManager The DomainManager for the server
 	 */
 	function init(domainManager) {
-		//	if (!domainManager.hasDomain("phantom")) {
-		domainManager.registerDomain("phantom", {
-			major: 0,
-			minor: 1
-		});
-		//	}
+		if (!domainManager.hasDomain("phantom")) {
+			domainManager.registerDomain("phantom", {
+				major: 0,
+				minor: 1
+			});
+		}
 		domainManager.registerCommand(
 			"phantom", // domain name
 			"phantomWebPage", // command name
 			phantomWebPage, // command handler function
-			false, // this command is synchronous in Node
+			true, // this command is synchronous in Node
 			"Returns the total or free memory on the user's system in bytes", [{
 				name: "total", // parameters
 				type: "string",
@@ -69,22 +52,6 @@
 				type: "number",
 				description: "amount of memory in bytes"
 		}]
-		);
-
-		domainManager.registerCommand(
-			"phantom", // domain name
-			"getMemory", // command name
-			cmdGetMemory, // command handler function
-			false, // this command is synchronous in Node
-			"Returns the total or free memory on the user's system in bytes", [{
-				name: "total", // parameters
-				type: "string",
-				description: "True to return total memory, false to return free memory"
-			}], [{
-				name: "memory", // return values
-				type: "number",
-				description: "amount of memory in bytes"
-			}]
 		);
 	}
 	exports.init = init;
